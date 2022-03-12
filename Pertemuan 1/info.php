1805a98e70ac30186470e6dab0c780485a62fd1b
justderrr <94819484+justderrr@users.noreply.github.com>
2022-03-12 19:01:46 +0700

    Pertemuan 1

---
+++ Pertemuan 1/info.php
@@ -0,0 +1,13 @@
+<?php
+    $nama = "Dery";
+
+    $boolean = true;
+
+    define('phi', 3.14);
+
+    $jari2 = 7;
+
+    $luas = phi * $jari2 * $jari2 ;
+
+    echo "Luas lingkaran adalah" . $luas ;
+?>
